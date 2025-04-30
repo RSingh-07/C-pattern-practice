750c1a7c5ce04bf8c0b28e75974deea827e246da
RSingh-07 <537riya@gmail.com>
2025-04-30 08:45:21 +0530

    Update pattern3_seq_alpha.c

--- pattern3_seq_alpha.c
+++ pattern3_seq_alpha.c
@@ -1,1 +1,19 @@
+// pattern3_seq_alpha.c
+#include <stdio.h>
+void pattern3(int n) {
+    char ch = 'A';
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%c ", ch++);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    pattern3(n);
+    return 0;
+}
 
