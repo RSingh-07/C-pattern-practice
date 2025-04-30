fbbb9bbaf018f61c54f9f1085fcb22eac737df1a
RSingh-07 <537riya@gmail.com>
2025-04-30 08:45:47 +0530

    Update pattern4_row_repeat.c

--- pattern4_row_repeat.c
+++ pattern4_row_repeat.c
@@ -1,1 +1,19 @@
 
+// pattern4_row_repeat.c
+#include <stdio.h>
+void pattern4(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    pattern4(n);
+    return 0;
+}
+
