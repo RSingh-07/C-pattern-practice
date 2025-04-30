c243717310664e01a6cf504375f520ddbc20cd6e
RSingh-07 <537riya@gmail.com>
2025-04-30 08:46:08 +0530

    Update pattern5_seq_numbers.c

--- pattern5_seq_numbers.c
+++ pattern5_seq_numbers.c
@@ -1,1 +1,19 @@
+// pattern5_seq_numbers.c
+#include <stdio.h>
+void pattern5(int n) {
+    int num = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", num++);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    pattern5(n);
+    return 0;
+}
 
